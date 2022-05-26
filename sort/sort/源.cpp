0bc0d4ac08433dbbac9664fdc834952fbdfba283
01294268442ww <92193285+01294268442ww@users.noreply.github.com>
2022-05-26 23:38:48 +0800

    Add files via upload

---
+++ sort/sort/源.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void InsertSort(int* a, int n)
+{
+	for (int i = 0; i < n-1; i++)
+	{
+		int tmp = a[i];
+		for (int j = i; j >= 0; j--)
+		{
+
+		}
+	}
+}
+
+int main()
+{
+
+}
