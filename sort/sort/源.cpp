7eb0ff877003d40c66989e16dc706a28f9c0262a
01294268442ww <92193285+01294268442ww@users.noreply.github.com>
2022-05-27 13:53:11 +0800

    Add files via upload

--- sort/sort/源.cpp
+++ sort/sort/源.cpp
@@ -4,15 +4,79 @@ void InsertSort(int* a, int n)
 {
 	for (int i = 0; i < n-1; i++)
 	{
-		int tmp = a[i];
-		for (int j = i; j >= 0; j--)
+		int end=i;
+		int tmp = a[end + 1];
+		while (end >= 0)
 		{
+			if (tmp < a[end])
+			{
+				a[end + 1] = a[end];
+				end--;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end+1] = tmp;
+	}
+}
 
+void Print(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
+void TestInserSort()
+{
+	int a[] = { 10,9,8,7,6,5,4,3,2,1 };
+	int len = sizeof(a) / sizeof(a[0]);
+	InsertSort(a, len);
+	Print(a, len);
+}
+
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
 		}
 	}
 }
 
+void TestShellSort()
+{
+	int a[] = { 10,9,8,7,6,5,4,3,2,1 };
+	int len = sizeof(a) / sizeof(a[0]);
+	ShellSort(a, len);
+	Print(a, len);
+}
+
 int main()
 {
-
+	//TestInserSort();
+	TestShellSort();
+	return 0;
 }
