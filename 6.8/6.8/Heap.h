a967862c143c951e21a8b95914b4dc7f8b129d33
01294268442ww <92193285+01294268442ww@users.noreply.github.com>
2022-06-09 16:45:35 +0800

    Add files via upload

--- 6.8/6.8/Heap.h
+++ 6.8/6.8/Heap.h
@@ -35,10 +35,11 @@ void AdjustUp(HPDataType* a, int child);
 
 void  HeapPrint(Heap* hp);
 
+void Swap(HPDataType* x, HPDataType* y);
 // TopK问题：找出N个数里面最大/最小的前K个问题。
 // 比如：未央区排名前10的泡馍，西安交通大学王者荣耀排名前10的韩信，全国排名前10的李白。等等问题都是Topk问题，
 // 需要注意：
 // 找最大的前K个，建立K个数的小堆
 // 找最小的前K个，建立K个数的大堆
-//void PrintTopK(int* a, int n, int k);
-//void TestTopk();
+
+
--- 6.8/6.8/test.c
+++ 6.8/6.8/test.c
@@ -64,13 +64,74 @@ void testHeap()
 	HeapDestory(&hp);
 }
 
+void PrintTopK(int* a, int n, int k)
+{
+
+	HPDataType* MinHeap = (HPDataType*)malloc(sizeof(HPDataType) * k);
+	//将a中前k个元素导入
+	for (int i = 0; i < k; i++)
+	{
+		MinHeap[i] = a[i];
+	}
+	
+	//以前k个元素建堆
+	for (int i = (k - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(MinHeap, i, k);
+	}
+
+	//剩下的n-k个元素和堆顶比较
+	for (int i = k; i < n; i++)
+	{
+		if (a[i] > MinHeap[0])
+		{
+			MinHeap[0] = a[i];
+			AdjustDown(MinHeap, 0, k);
+		}
+	}
+
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ", MinHeap[i]);
+	}
+	printf("\n");
+
+}
+
+void TestTopk()
+{
+	int n = 10000;
+	int* a = (int*)malloc(sizeof(int) * n);
+	srand(time(0));
+	//随机生成10000个数存入数组，保证元素都小于1000000
+	for (size_t i = 0; i < n; ++i)
+	{
+		a[i] = rand() % 1000000;
+	}
+	//确定10个最大的数
+	a[5] = 1000000 + 1;
+	a[1231] = 1000000 + 2;
+	a[531] = 1000000 + 3;
+	a[5121] = 1000000 + 4;
+	a[115] = 1000000 + 5;
+	a[2335] = 1000000 + 6;
+	a[9999] = 1000000 + 7;
+	a[76] = 1000000 + 8;
+	a[423] = 1000000 + 9;
+	a[3144] = 1000000 + 10;
+
+	PrintTopK(a, n, 10);
+}
+
 int main()
 {
 	//testStack();
 
 	//testQueue();
 
-	testHeap();
+	//testHeap();
+
+	TestTopk();
 
 	return 0;
 }
